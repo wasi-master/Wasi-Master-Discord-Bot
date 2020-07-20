2fcba1e813082a39ce01a10b7cee20d9f1bcd212
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-07-20 14:18:58 +0600

    update bot.py

--- bot.py
+++ bot.py
@@ -520,7 +520,7 @@ async def quiz(ctx):
 	quiz = True
 	def check(message = discord.Message):
 		if not message.author.bot:
-			return message.author == ctx.message.author and message.content.strip().lower() == correct_answer
+			return message.author == ctx.message.author and str(message.content).strip().lower() == correct_answer
 	
 	embed = discord.Embed()
 	embed.set_author(name=f"{ctx.author}\'s Quiz")
@@ -571,7 +571,7 @@ async def quiz(ctx):
 	else:
 		await ctx.message.channel.send('Correct you big brain')
 	finally:
-		await ctx.send("Stopped listening to that message\'s reactions")
+		await ctx.message.channel.send("Stopped listening to that message\'s reactions")
 @client.command()
 async def lol(ctx):
   await ctx.send("send your name in 69 seconds")
