c35da425f57c583fd9dd37ab9d90a78a193637f1
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-07-22 12:31:50 +0600

    update bot.py

--- bot.py
+++ bot.py
@@ -91,6 +91,7 @@ async def secretuserinfo(ctx, id: int=None):
 	embed.add_field(name="Account Created At", value=member.created_at.strftime("%a, %d %B %Y, %H:%M:%S"))
 	embed.add_field(name="Bot?", value=member.bot)
 	embed.add_field(name="Online Status", value=f"Desktop: {member.desktop_status}\nWeb: {member.web_status}\nMobile: {member.mobile_status}")
+	embed.add_field(name="Has Animated Avatar?", value=member.is_avatar_animated())
 	await ctx.send(embed=embed)
 	
 @client.command(aliases=["messagecount", "mc", "countmessages"])
