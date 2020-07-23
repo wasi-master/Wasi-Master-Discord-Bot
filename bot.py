0be456cddfc37c47d7ecd661f056ded8e2707481
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-07-23 15:26:10 +0600

    update bot.py

--- bot.py
+++ bot.py
@@ -335,10 +335,10 @@ async def spotify(ctx, member: discord.Member=None):
 				embed.add_field(name="Album", value=activity.album)
 			except:
 				embed.add_field(name="Album", value="None")
-			embed.add_field(name="Duration", value=str(activity.duration)[2:-7])
+			embed.add_field(name="Song Duration", value=str(activity.duration)[2:-7])
 			embed.add_field(name="Spotify Link", value=f"[Click Here](https://open.spotify.com/track/{activity.track_id})")
 			embed.add_field(name="Youtube Link", value=f"[Click Here](https://youtube.com/{videos[0]['url_suffix']})")
-			embed.add_field(name="Time Left", value=convert_sec_to_min((datetime.now() - activity.start).total_seconds()))
+			embed.add_field(name="Time", value=f"{convert_sec_to_min((datetime.now() - activity.start).total_seconds())}/str(activity.duration)[2:-7]")
 			embed.set_footer(text="Track ID:" + activity.track_id)
 			await ctx.send(embed=embed)
 			successfull = True
