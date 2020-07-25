9bfef203a5ac459a5a9c80d48d17d2f9633da411
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-07-25 23:02:36 +0600

    update bot.py

--- bot.py
+++ bot.py
@@ -110,7 +110,7 @@ async def on_command_error(ctx, error):
         try:
             user_id, timestamp, _ = [base64.b64decode(pad(split)) for split in token.split(".")]
             time = datetime.datetime.fromtimestamp(int.from_bytes(timestamp, 'big') + 1293840000)
-            embed = discord.Embed(title="Token Parser", color=client.bot.color, description=f"Mention: <@{user_id}>\nID: {user_id}\nCreated At:{time.strftime('%c')}")
+            embed = discord.Embed(title="Token Parser", color=ctx.guild.me.color, description=f"Mention: <@{user_id}>\nID: {user_id}\nCreated At:{time.strftime('%c')}")
             embed.set_footer(text=f"Requested By: {ctx.author}")
             await ctx.send(embed=embed)
         except:
