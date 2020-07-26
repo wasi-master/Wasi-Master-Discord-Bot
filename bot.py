7819a5a4ee285733d8d8e483e6e508a5cd6c9683
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-07-26 19:57:27 +0600

    update bot.py

--- bot.py
+++ bot.py
@@ -352,15 +352,10 @@ async def youtube(ctx, *, args):
 async def serverinfo(ctx):
 	guild = ctx.message.guild
 	owner = client.get_user(guild.owner_id)
-	embed=discord.Embed()
-	embed.set_author(name=guild.name)
-	embed.add_field(name='ID:', value=guild.id)
-	embed.add_field(name="Region", value=str(guild.region).capitalize())
-	embed.add_field(name="Emojis", value=len(guild.emojis))
-	embed.set_thumbnail(url=guild.icon_url)
-	embed.add_field(name="Owners ID", value=guild.owner_id)
-	embed.add_field(name="Owners Name", value=owner.name)
-	embed.add_field(name="Verification Level", value=str(guild.verification_level).capitalize())
+	features = ""
+	for i in guild.features:
+		features += "\n" + i.title()
+	embed=discord.Embed(title=f"Server Information for {guild.name}", description=f"Name: {guild.name}\nCreated At{guild.created_at.strftime('%a, %d %B %Y, %H:%M:%S'))}\nID: {guild.id}\nOwner: {owner}\nIcon Url: {guild.icon_url}\nRegion: {guild.region.calitalize()}\nVeirfication Level: {str(guild.verificitation_level).capitalize()}\n{len(guild.members)}\nBoost Level: {guild.premium_tier}\nBoosts: {guild.premium_subscription_count}\nBoosters: {len(guild.premium_subscribers)}\nTotal Channels: {len(guild.channels)}\nText Channels: {len(guild.text_channels)}\nVoice Channels: {len(guild.voice_channels)}\nCategories: {len(guild.categories)}\nRoles: {len(guild.roles)}\nEmojis: {len(guild.emojis)}/{guild.emoji_limit}\nUpload Limit: {round(guild.filesize_limit)}\nFeatures: {features}")
 	await ctx.send(embed=embed)
 @client.command()
 async def info(ctx):
