453cb7bf4a4337370af2e7886048e081d90c2003
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-07-26 20:00:39 +0600

    update bot.py

--- bot.py
+++ bot.py
@@ -355,7 +355,7 @@ async def serverinfo(ctx):
 	features = ""
 	for i in guild.features:
 		features += "\n" + i.title()
-	embed=discord.Embed(title=f"Server Information for {guild.name}", description=f"Name: {guild.name}\nCreated At{guild.created_at.strftime('%a, %d %B %Y, %H:%M:%S')}\nID: {guild.id}\nOwner: {owner}\nIcon Url: {guild.icon_url}\nRegion: {guild.region.calitalize()}\nVeirfication Level: {str(guild.verificitation_level).capitalize()}\n{len(guild.members)}\nBoost Level: {guild.premium_tier}\nBoosts: {guild.premium_subscription_count}\nBoosters: {len(guild.premium_subscribers)}\nTotal Channels: {len(guild.channels)}\nText Channels: {len(guild.text_channels)}\nVoice Channels: {len(guild.voice_channels)}\nCategories: {len(guild.categories)}\nRoles: {len(guild.roles)}\nEmojis: {len(guild.emojis)}/{guild.emoji_limit}\nUpload Limit: {round(guild.filesize_limit)}\nFeatures: {features}")
+	embed=discord.Embed(title=f"Server Information for {guild.name}", description=f"Name: {guild.name}\nCreated At{guild.created_at.strftime('%a, %d %B %Y, %H:%M:%S')}\nID: {guild.id}\nOwner: {owner}\nIcon Url: {guild.icon_url}\nRegion: {str(guild.region).calitalize()}\nVeirfication Level: {str(guild.verificitation_level).capitalize()}\n{len(guild.members)}\nBoost Level: {guild.premium_tier}\nBoosts: {guild.premium_subscription_count}\nBoosters: {len(guild.premium_subscribers)}\nTotal Channels: {len(guild.channels)}\nText Channels: {len(guild.text_channels)}\nVoice Channels: {len(guild.voice_channels)}\nCategories: {len(guild.categories)}\nRoles: {len(guild.roles)}\nEmojis: {len(guild.emojis)}/{guild.emoji_limit}\nUpload Limit: {round(guild.filesize_limit)}\nFeatures: {features}")
 	await ctx.send(embed=embed)
 @client.command()
 async def info(ctx):
