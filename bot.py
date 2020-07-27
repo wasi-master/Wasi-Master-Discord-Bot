a017a5a838fecef63ee475606300e6c99c0d6e63
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-07-27 09:26:21 +0600

    update bot.py

--- bot.py
+++ bot.py
@@ -117,7 +117,7 @@ async def covid(ctx, area: str="Global"):
 	if not area.lower() == "global":
 		for i in formatted_json['Countries']:
 			num += 1
-			if i["Slug"] == area:
+			if i["Slug"] == area.lower:
 				formatted_json = i[num]
 				await ctx.send(formatted_json)
 				break
