284f16e0aeabc5ce450920ee3e95e080e184c1b2
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-07-29 23:12:40 +0600

    update bot.py

--- bot.py
+++ bot.py
@@ -124,14 +124,14 @@ async def truthordate(ctx, questype: str= "random"):
 async def wanted(ctx, member: discord.Member=None):
 	member = member or ctx.message.author
 	session = aiohttp.ClientSession()
-	headers = {'token':'VWTwUej1JzUQ1iAPjeZUNOavwlX3EIeOHtSfskjNDtIODoYugLxBNcHFEHMqiJtB', 'url': str(ctx.message.author.avatar_url)}
+	headers = {'token':'VWTwUej1JzUQ1iAPjeZUNOavwlX3EIeOHtSfskjNDtIODoYugLxBNcHFEHMqiJtB', 'url': str(member.avatar_url)}
 	async with ctx.typing():
 		async with session.post("https://dagpi.tk/api/wanted", headers=headers) as response:
 			loaded_response = await response.text()
 		formatted_json = json.loads(loaded_response)
 		session.close()
 	if formatted_json['succes']:
-		embed = discord.Embed(title=f"{ctx.message.author.name} Wanted")
+		embed = discord.Embed(title=f"{member.name} Wanted")
 		embed.set_image(url=formatted_json["url"])
 		await ctx.send(embed=embed)
 	else:
