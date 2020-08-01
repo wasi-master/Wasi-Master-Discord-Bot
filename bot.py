cafe880f5cc06d3fc7bd96ddea43725e037f3875
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-08-01 21:34:28 +0600

    update bot.py

--- bot.py
+++ bot.py
@@ -637,7 +637,7 @@ async def colour(ctx, color: str):
 		hsl = data["hsl"]["value"]
 		hsv = data["hsv"]["value"]
 		cmyk = data["cmyk"]["value"]
-		xyz = data["xyz"]["valuel"]
+		xyz = data["XYZ"]["valuel"]
 		
 	embed = discord.Embed(timestamp=ctx.message.created_at, color=int(hex.replace("#", ""), 16))
 	embed.set_author(name=color_name)
