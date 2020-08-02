d55ed0fd8c255a8aa6770bb90200ad5f8e96584d
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-08-02 10:40:29 +0600

    update bot.py

--- bot.py
+++ bot.py
@@ -1010,9 +1010,9 @@ async def help(ctx, command: str=None):
 				embed.add_field(name="Description", value=command_for_use.description)
 			except:
 				pass
-			try:
+			if not len(aliases) == 0
 				embed.add_field(name="Aliases", value=aliases[:-2])
-			except:
+			else:
 				pass
 			embed.add_field(name="Cooldown", value="None")
 			await ctx.send(embed=embed)
