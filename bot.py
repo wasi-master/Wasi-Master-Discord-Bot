e2c1b30d57a1bdd01bfeef6148f4996897330c2e
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-08-05 02:40:31 +0600

    update bot.py

--- bot.py
+++ bot.py
@@ -209,11 +209,9 @@ async def screenshot(ctx, website:str):
             headers={"website": website}
         ) as response:
             data = await response.json()
-            text = await response.text()
             embed = discord.Embed(title=data["website"], url=website)
             embed.set_image(url=data["snapshot"])
         await ctx.send(embed=embed)
-        await ctx.send(text)
         session.close()
 
 
