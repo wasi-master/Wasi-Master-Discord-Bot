d841cd34cef300b5f44c277837ee900429179035
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-08-04 22:53:52 +0600

    update bot.py

--- bot.py
+++ bot.py
@@ -574,7 +574,7 @@ async def boosters(ctx):
 	peoples = []
 	for i in ctx.message.guild.premium_subscribers:
 		peoples.append(i.name)
-	await paginator()
+	await paginator(ctx, peoples)
 	
 
 @client.command(description="Invert your or another users profile picture")
