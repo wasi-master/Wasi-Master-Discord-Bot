52c7ee63d50e31b1ce7fa16026092a18c7271cfc
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-08-15 11:39:36 +0600

    update bot.py

--- bot.py
+++ bot.py
@@ -198,7 +198,7 @@ async def on_command_error(ctx, error):
     elif isinstance(error, commands.MissingPermissions):
         await ctx.send("I can't do that")
     elif isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send(f"The {error.param} argument is missing")
+        await ctx.send(f"The {str(error.param).split(":")[0].strip()} argument is missing")
     elif isinstance(error, commands.CommandNotFound):
         pass
     elif isinstance(error, discord.Forbidden):
@@ -249,6 +249,95 @@ async def on_command_error(ctx, error):
 
 def pad(to_pad):
     return to_pad + "=" * ((4 - len(to_pad) % 4) % 4)
+
+
+@client.command(description="Morse code :nerd:")
+async def morse(ctx, text:str):
+    MORSE_CODE_DICT = { 'A':'.-', 'B':'-...', 
+    
+                        'C':'-.-.', 'D':'-..', 'E':'.', 
+    
+                        'F':'..-.', 'G':'--.', 'H':'....', 
+    
+                        'I':'..', 'J':'.---', 'K':'-.-', 
+    
+                        'L':'.-..', 'M':'--', 'N':'-.', 
+    
+                        'O':'---', 'P':'.--.', 'Q':'--.-', 
+    
+                        'R':'.-.', 'S':'...', 'T':'-', 
+    
+                        'U':'..-', 'V':'...-', 'W':'.--', 
+    
+                        'X':'-..-', 'Y':'-.--', 'Z':'--..', 
+    
+                        '1':'.----', '2':'..---', '3':'...--', 
+    
+                        '4':'....-', '5':'.....', '6':'-....', 
+    
+                        '7':'--...', '8':'---..', '9':'----.', 
+    
+                        '0':'-----', ', ':'--..--', '.':'.-.-.-', 
+    
+                        '?':'..--..', '/':'-..-.', '-':'-....-', 
+    
+                        '(':'-.--.', ')':'-.--.-'}
+    cipher = '' 
+    for letter in message: 
+        if letter != ' ': 
+            cipher += MORSE_CODE_DICT[letter] + ' '
+        else: 
+            cipher += ' '
+    await ctx.send(embed=discord.Embed(title=str(ctx.author), description=cipher))
+
+
+@client.command(description="English to morse")
+async def unmorse(ctx, text:str):
+    MORSE_CODE_DICT = { 'A':'.-', 'B':'-...', 
+    
+                        'C':'-.-.', 'D':'-..', 'E':'.', 
+    
+                        'F':'..-.', 'G':'--.', 'H':'....', 
+    
+                        'I':'..', 'J':'.---', 'K':'-.-', 
+    
+                        'L':'.-..', 'M':'--', 'N':'-.', 
+    
+                        'O':'---', 'P':'.--.', 'Q':'--.-', 
+    
+                        'R':'.-.', 'S':'...', 'T':'-', 
+    
+                        'U':'..-', 'V':'...-', 'W':'.--', 
+    
+                        'X':'-..-', 'Y':'-.--', 'Z':'--..', 
+    
+                        '1':'.----', '2':'..---', '3':'...--', 
+    
+                        '4':'....-', '5':'.....', '6':'-....', 
+    
+                        '7':'--...', '8':'---..', '9':'----.', 
+    
+                        '0':'-----', ', ':'--..--', '.':'.-.-.-', 
+    
+                        '?':'..--..', '/':'-..-.', '-':'-....-', 
+    
+                        '(':'-.--.', ')':'-.--.-'}
+    message += ' '
+    decipher = '' 
+    citext = '' 
+    for letter in message: 
+        if (letter != ' '): 
+            i = 0
+            citext += letter
+        else: 
+            i += 1
+            if i == 2 : 
+                decipher += ' '
+            else: 
+                decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT 
+                .values()).index(citext)] 
+                citext = '' 
+    await ctx.send(embed=discord.Embed(title=str(ctx.author), description=decipher)
 
 
 @client.command(description="Check who got banned")
