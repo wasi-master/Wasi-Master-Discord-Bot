fe9d69673f6f821b6dbfc293015666246daed516
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-08-21 01:24:06 +0600

    update bot.py

--- bot.py
+++ bot.py
@@ -462,9 +462,9 @@ async def top(ctx, limit = 500, *, channel: discord.TextChannel = None):
       
 @client.command(description="Do math stuff")
 async def math(ctx, equation:str):
-    available = [" ", "*", "^", "+", "-", "/"]
+    available = ["1", "2", "3", "4", "5", "6", "7", "8", "9', "0", " ", *", "^", "+", "-", "/"]
     for i in equation:
-        if not i.isdigit() or i in available:
+        if not i in available:
             return await ctx.send("Invalid Math Equation")
     if not len(equation) > 15 and not equation.count("**") > 1 and not equation.count("^") < 1:
         try:
