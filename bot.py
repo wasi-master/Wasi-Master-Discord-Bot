6cd4cf15b0ee1353c1e2477285b0819950572f14
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-08-20 14:59:34 +0600

    update bot.py

--- bot.py
+++ bot.py
@@ -2350,7 +2350,10 @@ async def helpcommand(ctx, command: str = None):
                 embed.add_field(name="Usage", value=ctx.prefix + command_for_use.usage)
             else:
                 embed.add_field(name="Usage", value=ctx.prefix + command_for_use.name + " " + " ".join([f"`{i}`" for i in client.get_command(command_for_use.name).signature.split(" ")]))
-            embed.add_field(name="Cooldown", value="None")
+            if not command_for_use._buckets._cooldown is None:
+                embed.add_field(name="Cooldown", value="None")
+            else:
+                embed.add_field(name="Cooldown", value=f"{command_for_use._buckets._cooldown.rate} seconds ({humanize.naturaldelta(datetime.timedelta(seconds=int(command_for_use._buckets._cooldown.rate)))})")
             await ctx.send(embed=embed)
         else:
             try:
