fdacd774d7bc4e1da60b05fe6acbde6113204ac1
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-08-21 10:39:13 +0600

    update bot.py

--- bot.py
+++ bot.py
@@ -37,6 +37,53 @@ import urllib.parse
 
 
 
+async def paginator (ctx , entries , limit=5 ):
+    pages = []
+    my_list = []
+    for i in entries :
+        my_list.append(i)
+    for i in range(0 , (len(my_list)) , int(limit)):
+          pages.append(tuple(my_list[i:i+int(limit)]))
+    c = pages
+
+    content = ""
+    for i in c[0]:
+       content += str(i) + "\n"
+    k = await ctx.send(f"`​``{content}`​``")
+    def check (reaction , user) :
+          return user == ctx.author and reaction.message.id == k.id
+    reactions = [ ":backward:",":stop:", ":forward:" ]
+    prev ,stop ,  next = reactions
+    for i in reactions :
+         await k.add_reaction(i)
+    pages = 0
+    while True :
+       try : 
+            a = await  ctx.bot.wait_for("reaction_add" , timeout = 45 , check = check)
+            if str(a[0]) == next:
+                   pages += 1 
+                   content = ""
+                   for i in c[pages]:
+                         content += str(i) + "\n"
+                   await k.edit(content = f"`​``{content}`​``")
+                   continue
+            if str(a[0]) == prev :
+                  pages -= 1 
+                  content = ""
+                  for i in c[pages]:
+                         content += str(i) + "\n"
+                  await k.edit(content = f"`​``{content}`​``")
+            if str(a[0]) == stop :
+               await k.delete()
+               break
+       except(asyncio.TimeoutError):
+            break
+       except (IndexError):
+            pass
+
+
+
+
 
 async def get_prefix(client, message):
     if isinstance(message.channel, discord.DMChannel):
@@ -2429,7 +2476,7 @@ async def wikipedia(ctx, *, args):
         if len(result) < 1997:
             await ctx.send(result)
         else:
-            await ctx.send(result[0:1997] + "...")
+            await paginator(ctx, result.split("\n"), 20)
 
 
 @client.command(
