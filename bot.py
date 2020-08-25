daa4f9bd06e7a601e073d51ccf56e1586a488c1a
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-08-25 19:24:39 +0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -2593,8 +2593,7 @@ async def avatar(
 async def helpcommand(ctx, command: str = None):
     all_commands = ""
     if command is None:
-        for i in sorted([i.name for i in client.commands]):
-            all_commands += f"`{i}`, "
+        all_commands += sorted([f"`{i.name}`, " for i in client.commands])
         if ctx.guild is None:
             color = 0x2f3136
         else:
