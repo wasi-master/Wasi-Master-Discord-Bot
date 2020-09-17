62994cc66fdadd9ac905352b94cd30d34b108c3b
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-09-17 23:39:14 +0600

    update bot.py

--- bot.py
+++ bot.py
@@ -278,7 +278,7 @@ async def create_db_pool():
     client.db = await asyncpg.create_pool(host="ec2-52-23-86-208.compute-1.amazonaws.com", database="d5squd8cvojua1", user="poladbevzydxyx", password="5252b3d45b9dd322c3b67430609656173492b3c97cdfd5ce5d9b8371942bb6b8")
 client.loop.run_until_complete(create_db_pool())
  
-@client.event
+#  @client.event
 async def fake_on_ready():
     await tracker.cache_invites()
     print("Bot is online")
