42f7f9ceae790163e16bc6ce6df3c8efdd2aa242
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-09-20 21:49:52 +0600

    update bot.py

--- bot.py
+++ bot.py
@@ -597,7 +597,7 @@ async def saveallemojis(ctx):
         await item.save(gn + "/" + name + ext)
         if done // 25 == 0:
             time_required = 0.038924*(len(emojis)-done)
-            embed = discord.Embed(title="Saving <a:typing:597589448607399949>", description=f"This should take {round(time_required, 2)} more seconds if all things go right")")
+            embed = discord.Embed(title="Saving <a:typing:597589448607399949>", description=f"This should take {round(time_required, 2)} more seconds if all things go right")
             embed.add_field(name="Progress", value=f"{done} {get_p(done/(len(emojis)/100))} {len(emojis)}")
             await msg.edit(embed=embed)
     embed = discord.Embed(title="Zipping <a:typing:597589448607399949>", description=f"This should take a few more seconds if all things go right")
