2d240a6d9566b4ca47f56697afd35a62769a051f
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-09-24 13:36:46 +0600

    update bot.py

--- bot.py
+++ bot.py
@@ -4060,7 +4060,7 @@ async def purge(ctx, amount: int, member: discord.Member = None):
     await ctx.message.delete()
     if not member:
         def reaction_check(r, u):
-            return r.channel.id == ctx.channel.id and ((u.author.id == ctx.author.id) or u.author.permissions.manage_messages)
+            return (not u.bot) and r.channel.id == ctx.channel.id and ((u.author.id == ctx.author.id) or u.author.permissions.manage_messages)
         deleted = await ctx.channel.purge(limit=amount)
         a = "message" if len(deleted) else "messages"
         spammers = Counter(m.author.display_name for m in deleted)
