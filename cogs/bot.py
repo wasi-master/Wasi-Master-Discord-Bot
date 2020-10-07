b46fdc53a13100499e96fc4ef6765178c2469354
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-10-07 12:35:28 +0600

    update bot.py

--- cogs/bot.py
+++ cogs/bot.py
@@ -308,7 +308,8 @@ class PaginatedHelpCommand(commands.HelpCommand):
 
     async def common_command_formatting(self, embed_like, command):
         embed_like.title = self.get_command_signature(command)
-        embed_like.add_field(name="Cooldown", value=f"{command._buckets._cooldown.per} seconds per {command._buckets._cooldown.rate} commands per {str(command._buckets._cooldown.type).split('.', '')[1]}")
+        if command._buckets.cooldown:
+            embed_like.add_field(name="Cooldown", value=f"{command._buckets._cooldown.per} seconds per {command._buckets._cooldown.rate} commands per {str(command._buckets._cooldown.type).split('.', '')[1]}")
         command_usage = await self.bot.db.fetchrow("""
                     SELECT *
                     FROM usages
