77ea477e1dc2c65b604b0e508bd8a2ede66cfa22
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-10-24 14:51:12 +0600

    update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -23,7 +23,11 @@ class Fun(commands.Cog):
         if snipes.get(channel.id):
             message = snipes[channel.id]
             e = discord.Embed(title="Said:", description=message.content, timestamp=message.created_at, color=discord.Colour.green())
-            e.set_author(icon_url=message.author.avatar_url, name=str(message.author))
+            if message.author.display_name == message.author.name:
+                name = str(message.author)
+            else:
+                name = f"{message.author} ({message.author.display_name})"
+            e.set_author(icon_url=message.author.avatar_url, name=name)
             e.set_thumbnail(url=message.author.avatar_url)
             await ctx.send(embed=e)
         else:
