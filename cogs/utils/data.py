dcd1faeada9a01f87c60d20795f31fb1ed27074a
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-10-24 22:59:09 +0600

    update data.py

--- cogs/utils/data.py
+++ cogs/utils/data.py
@@ -2,6 +2,6 @@ from classes import ShopItem
 
 # name, pretty_name, description buy, sell, is_usable
 shop_items = [
-    ShopItem("trophy", "Trophy", "", 5000, 3000, False),
-    ShopItem("banana", "Banana", "", 300, 100, True)
+    ShopItem("trophy", "Trophy", "Trophie to show off to people that ur rich", 10000, 8000, False),
+    ShopItem("banana", "Banana", "All-mighty banana that does basically nothing but give you some coins", 300, 100, True)
     ]
