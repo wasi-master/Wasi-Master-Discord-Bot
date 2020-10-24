1ae75960219ed3826a91dea5e54e43442cc8268e
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-10-24 22:44:50 +0600

    update classes.py

---
+++ cogs/utils/classes.py
@@ -0,0 +1,6 @@
+class ShopItem():
+    def __init__(self, name, beautiful_name, buy, sell):
+        self.name = name
+        self.pretty_name = beautiful_name
+        self.buy = buy
+        self.sell = sell
