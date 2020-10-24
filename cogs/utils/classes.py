c0da0cacbc5b9917528391f2707d7369b64412d6
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-10-24 22:53:51 +0600

    update classes.py

--- cogs/utils/classes.py
+++ cogs/utils/classes.py
@@ -1,5 +1,5 @@
 class ShopItem():
-    def __init__(self, name: str, beautiful_name: str, buy: int, sell: int, is_usable: bool):
+    def __init__(self, name: str, beautiful_name: str, description: str, buy: int, sell: int, is_usable: bool):
         self.name = name
         self.pretty_name = beautiful_name
         self.buy = buy
