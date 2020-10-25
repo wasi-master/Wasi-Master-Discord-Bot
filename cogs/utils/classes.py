f036cab58eec169ccfd0ff6f58d5809005cbe804
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2020-10-25 17:36:02 +0600

    update classes.py

--- cogs/utils/classes.py
+++ cogs/utils/classes.py
@@ -1,7 +1,13 @@
 
+
 class ShopItem():
     def __init__(self, name: str, beautiful_name: str, description: str, buy: int, sell: int, is_usable: bool):
         self.name = name
         self.pretty_name = beautiful_name
         self.buy = buy
         self.sell = sell
+
+class Embed(discord.Embed):
+  def __init__(self, **kwargs):
+    self.colour = kwargs.get(colour) or 0x36393E
+    super().__init__(kwargs)
